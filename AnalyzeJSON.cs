998a7c7b48b830faadb6d4ff8c8b2c32955710bc
Piotr Czajkowski <pczajkowski@gmail.com>
2022-01-08 12:33:47 +0100

    Starting with it

---
+++ AnalyzeJSON.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace analyzeJSON
+{
+    public class AnalyzeJSON
+    {
+        private readonly JObject json;
+
+        public AnalyzeJSON(string path)
+        {
+            if (!File.Exists(path))
+                throw new ArgumentNullException("File doesn't exist!");
+
+            using StreamReader sr = new StreamReader(path);
+            var jsonString = sr.ReadToEnd();
+            json = JsonConvert.DeserializeObject<JObject>(jsonString);
+        }
+    }
+}
---
+++ analyzeJSONTests/AnalyzeJSONIntegrationTests.cs
@@ -0,0 +1,16 @@
+﻿using analyzeJSON;
+using Xunit;
+
+namespace analyzeJSONTests
+{
+    public class AnalyzeJSONIntegrationTests
+    {
+        private static readonly string testFile = @"testFiles/complex.json";
+
+        [Fact]
+        public void AnalyzeJSON_AllGood()
+        {
+            var test = new AnalyzeJSON(testFile);
+        }
+    }
+}
