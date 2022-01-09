03670a4d4b958a06b2c55bbe9febd7d2312ae65a
Piotr Czajkowski <pczajkowski@gmail.com>
2022-01-09 18:48:38 +0100

    Added GetNameFromPath_DifferentCases

--- analyzeJSONTests/AnalyzeJSONUnitTests.cs
+++ analyzeJSONTests/AnalyzeJSONUnitTests.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using analyzeJSON;
 using Xunit;
 
 namespace analyzeJSONTests
 {
+    public record TestCase(string Input, string ExpectedOutput);
+
     public class AnalyzeJSONUnitTests
     {
         [Fact]
@@ -22,6 +25,22 @@ namespace analyzeJSONTests
         public void GetNameFromPath_EmptyTokenPath()
         {
             Assert.Empty(AnalyzeJSON.GetNameFromPath(string.Empty));
+        }
+
+        [Fact]
+        public void GetNameFromPath_DifferentCases()
+        {
+            var testCases = new List<TestCase>
+            {
+                new TestCase("abc", "abc"),
+                new TestCase("abc.def", "def"),
+                new TestCase("abc.", string.Empty),
+                new TestCase("abc.def.", string.Empty),
+
+            };
+
+            foreach (var testCase in testCases)
+                Assert.Equal(testCase.ExpectedOutput, AnalyzeJSON.GetNameFromPath(testCase.Input));
         }
     }
 }
