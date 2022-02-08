ac1a7368d65dfa843a54ff4fbd1eae750040ef34
Piotr Czajkowski <pczajkowski@gmail.com>
2022-02-08 20:13:35 +0100

    Added ExtractText_NullKeys

---
+++ analyzeJSON/ExtractText.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace analyzeJSON
+{
+    public class ExtractText
+    {
+        private readonly Dictionary<string, bool> keysToExtract;
+
+        public ExtractText(Dictionary<string, bool> keys)
+        {
+            keysToExtract = keys ?? throw new ArgumentNullException(nameof(keys));
+        }
+
+        public void Extract(JToken token)
+        {
+            if (token.Type != JTokenType.String)
+                return;
+
+            var tokenName = AnalyzeJSON.GetNameFromPath(token.Path);
+            if (string.IsNullOrWhiteSpace(tokenName))
+                return;
+
+            if (!keysToExtract.ContainsKey(tokenName))
+                return;
+        }
+    }
+}
---
+++ analyzeJSONTests/ExtractTextUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using analyzeJSON;
+using System;
+
+namespace analyzeJSONTests
+{
+    public class ExtractTextUnitTests
+    {
+        [Fact]
+        public void ExtractText_NullKeys()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ExtractText(null));
+        }
+    }
+}
